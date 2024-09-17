d857b178739486fd57ed6ca7644f9f703976e1d8
MinhPhuc-Dev <nguyenhoangminhphuc2503@gmail.com>
2024-09-17 11:11:45 +0700

    track.lua

--- track.lua
+++ track.lua
@@ -14,7 +14,7 @@ end
 -- Hàm gửi thông tin đến Webhook.site
 local function sendToWebhook(stats)
     -- URL Webhook.site mà bạn đã lấy ở Bước 1
-    local proxyUrl = "https://webhook.site/xxxx-xxxx-xxxx-xxxx" -- Thay bằng URL Webhook.site của bạn
+    local proxyUrl = "https://webhook.site/3dae2b45-77d7-4f7b-a8ae-2e82f877e869" -- Thay bằng URL Webhook.site của bạn
     local data = {
         ["AccountName"] = stats.AccountName,
         ["UserId"] = stats.UserId,
